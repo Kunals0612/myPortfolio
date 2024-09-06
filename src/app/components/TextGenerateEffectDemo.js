fe56ee8482d6f72e1f3f4008118dc1526f762ae3
Kunal <kunalshinde0612@gmail.com>
2024-09-06 19:50:46 +0530

    commit

--- src/app/components/TextGenerateEffectDemo.js
+++ src/app/components/TextGenerateEffectDemo.js
@@ -5,5 +5,5 @@ const words = `UI/UX designer and full-stack dev, making cool stuff that works.
 `;
 
 export function TextGenerateEffectDemo() {
-  return <TextGenerateEffect words={words} />;
+  return <TextGenerateEffect words={words}  className='px-4'/>;
 }

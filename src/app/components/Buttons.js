9486c93cfedfa815b1a797d1ae399dca2236cf1d
Kunal <kunalshinde0612@gmail.com>
2024-09-06 19:16:44 +0530

    commit

--- src/app/components/Buttons.js
+++ src/app/components/Buttons.js
@@ -5,11 +5,11 @@ import Link from 'next/link'
 export function Buttons(props) {
   return (
     (<div className="btns flex flex-row gap-5 relative left-[34%]">
-      {/* <Link href="/Projects"><Button
+      <Link href="/Projects"><Button
         borderRadius="1.75rem"
         className="bg-white dark:bg-slate-800 text-black dark:text-white border-neutral-200 dark:border-violet-900">
         Projects
-      </Button></Link> */}
+      </Button></Link>
       <div>
       <Link href="/Contact"><Button
         borderRadius="1.75rem"
